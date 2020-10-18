ea58186cc0187f10d6c8221f737e7edaf4aa68ae
Aleksandar Sotirovic <aleksandar.sotirovic@gmail.com>
2020-10-18 14:46:10 +0200

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `My Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Sasha13`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
